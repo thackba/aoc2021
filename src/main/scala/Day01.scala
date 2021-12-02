050c43f9126e7ac56fe5f5f9a5f5db059c039e71
Thomas Hackbarth <thomas.hackbarth@codecentric.de>
2021-12-02 09:27:43 +0100

    improve day 1

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,12 +1,4 @@
-import scala.annotation.tailrec
 import scala.io.Source
-
-@tailrec
-def applySlidingWindow(rest: Seq[Int], acc: Seq[Int]): Seq[Int] =
-  if rest.length < 3 then
-    acc
-  else
-    applySlidingWindow(rest.tail, acc :+ rest.take(3).sum)
 
 @main def day01(): Unit =
   val input = Utils.readInput("day01-1-real.txt")
@@ -15,7 +7,8 @@ def applySlidingWindow(rest: Seq[Int], acc: Seq[Int]): Seq[Int] =
   // ### Solution 1
   // val data = intSeq
   // ### Solution 2
-  val data = applySlidingWindow(intSeq, Seq.empty[Int])
+  val data = intSeq.sliding(3).map(l => l.sum).toSeq
 
   val zipped = data.zip(Int.MaxValue +: data).count(x => x._1 > x._2)
+
   println(zipped)
